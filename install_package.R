d4ff08b1f83464cc57d625578623448e2e0c384d
jaesungc <jaesungc.econ@gmail.com>
2020-01-03 17:05:21 -0500

    Update install_package.R

--- install_package.R
+++ install_package.R
@@ -1,4 +1,4 @@
-# Package list to have
+# Packages to be installed
 package.list <- 
   c('dplyr','ggplot2','data.table','stringr','XML','rvest','httr','tidyverse')
 
@@ -6,17 +6,5 @@ package.list <-
 not.found.packages <- 
   package.list[!package.list %in% installed.packages()[,1]]
 
-# Set repos : cran.rstudio.com
-repos <- c('https://cran.rstudio.com/')
-names(repos) <- 'CRAN'
-
 # install.packages
-sapply(
-  not.found.packages, 
-  function(x) install.packages(pkgs=x, repos=repos, quiet=TRUE))
-
-# load packages
-sapply(
-  package.list, 
-  function(x) library(x, character.only=TRUE))
-
+install.packages(not.found.packages)
