cab35b6b1e290f75453da36be3a7d5298e459a2d
jaesungc <jaesungc.econ@gmail.com>
2020-04-04 18:31:16 -0400

    Delete R Sample Code.R

--- R Sample Code.R
+++
@@ -1,6 +0,0 @@
-# Remove some rows which satisfying conditions
-Stack.final<-Stack %>% filter(x1!="합계")
-
-# Replace all commas into empty
-myFun <- function(x) {gsub(",","",x)}
-D2<-apply(D1, 2, myFun) %>% data.frame()
